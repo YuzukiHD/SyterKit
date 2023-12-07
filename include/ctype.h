871845c4e2fa34dc0973eff50ef5a636c3b43b82
user <mali@mali.com>
2023-12-07 16:25:43 +0800

    add ctype

---
+++ include/ctype.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef __CTYPE_H__
+#define __CTYPE_H__
+
+int isalnum(int c);
+
+int isalpha(int c);
+
+int isascii(int c);
+
+int isblank(int c);
+
+int iscntrl(int c);
+
+int isdigit(int c);
+
+int isgraph(int c);
+
+int islower(int c);
+
+int isprint(int c);
+
+int ispunct(int c);
+
+int isspace(int c);
+
+int isupper(int c);
+
+int isxdigit(int c);
+
+int toascii(int c);
+
+int tolower(int c);
+
+int toupper(int c);
+
+#endif /* __CTYPE_H__ */
