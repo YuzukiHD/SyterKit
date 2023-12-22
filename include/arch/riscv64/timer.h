968d185a3538e55394c683dfabb7aab6e829eaf9
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-12-23 00:12:40 +0800

    [arch] add riscv 64

---
+++ include/arch/riscv64/timer.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef __SYS_TIMER_H__
+#define __SYS_TIMER_H__
+
+#include <io.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <types.h>
+
+#include "log.h"
+
+uint64_t get_arch_counter(void);
+
+uint32_t time_ms(void);
+
+uint64_t time_us(void);
+
+void udelay(uint64_t us);
+
+void mdelay(uint32_t ms);
+
+void sdelay(uint32_t loops);
+
+#endif// __SYS_TIMER_H__
