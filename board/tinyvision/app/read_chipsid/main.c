90faf531ca5949889e65e10ec7aac285cb5653a7
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-12-22 23:19:15 +0800

    merge apps from 0.1.2

--- board/tinyvision/app/read_chipsid/main.c
+++ board/tinyvision/app/read_chipsid/main.c
@@ -7,11 +7,15 @@
 
 #include <log.h>
 
+#include <common.h>
+
+#include <sys-sid.h>
+
 sunxi_serial_t uart_dbg = {
         .base = 0x02500000,
         .id = 0,
-        .gpio_tx = {GPIO_PIN(GPIO_PORTH, 9), GPIO_PERIPH_MUX5},
-        .gpio_rx = {GPIO_PIN(GPIO_PORTH, 10), GPIO_PERIPH_MUX5},
+        .gpio_tx = {GPIO_PIN(PORTH, 9), GPIO_PERIPH_MUX5},
+        .gpio_rx = {GPIO_PIN(PORTH, 10), GPIO_PERIPH_MUX5},
 };
 
 int main(void) {
@@ -19,7 +23,7 @@ int main(void) {
 
     sunxi_clk_init();
 
-    printk(LOG_LEVEL_INFO, "Hello World!\n");
+    dump_efuse();
 
     return 0;
 }
