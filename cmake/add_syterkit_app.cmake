a8ea528ce7dab971d5eada6e4f53aa3869fe7983
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-12-23 13:23:28 +0800

    [feat] support board function

--- cmake/add_syterkit_app.cmake
+++ cmake/add_syterkit_app.cmake
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
+# List of common libraries used by the application
 set(APP_COMMON_LIBRARY 
     fatfs
     fdt
@@ -8,12 +9,16 @@ set(APP_COMMON_LIBRARY
     gcc
 )
 
+# Function to add a new SyterKit application
 function(add_syterkit_app target_name)
+    # Add an executable for the target_name with _fel suffix
     add_executable(${target_name}_fel ${APP_COMMON_SOURCE} ${ARGN})
 
+    # Set properties for the target, including link dependencies and libraries
     set_target_properties(${target_name}_fel PROPERTIES LINK_DEPENDS "${LINK_SCRIPT_FEL}")
     target_link_libraries(${target_name}_fel ${APP_COMMON_LIBRARY} -T"${LINK_SCRIPT_FEL}" -nostdlib -Wl,-z,noexecstack,-Map,${target_name}_fel.map)
 
+    # Add custom commands for post-build actions
     add_custom_command(
         TARGET ${target_name}_fel
         POST_BUILD COMMAND ${CMAKE_SIZE} -B -x ${target_name}_fel
@@ -26,6 +31,7 @@ function(add_syterkit_app target_name)
         COMMENT "Copy Binary ${target_name}_fel"
     )
 
+    # Repeat the above steps for the _bin version of the target
     add_executable(${target_name}_bin ${APP_COMMON_SOURCE} ${ARGN})
 
     set_target_properties(${target_name}_bin PROPERTIES LINK_DEPENDS "${LINK_SCRIPT_BIN}")
