b6bfa81f7797a7634185d224d40dd3737f5a94cb
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-12-25 14:10:19 +0800

    add c906 loader

---
+++ board/100ask-t113i/load_c906/c906_firmware/include/string.h
@@ -0,0 +1,9 @@
+#ifndef __STRING_H__
+#define __STRING_H__
+
+#include <types.h>
+
+void *memset(void *s, int c, size_t n);
+void *memcpy(void *dest, const void *src, size_t len);
+
+#endif /* __STRING_H__ */
