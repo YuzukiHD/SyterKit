217debc53dfac138e14f37e759f6d750c27567ec
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-12-25 14:37:44 +0800

    fix T113-S3 Board uart

--- board/100ask-t113s3/board.c
+++ board/100ask-t113s3/board.c
@@ -15,13 +15,14 @@
 #include <sys-gpio.h>
 #include <sys-spi.h>
 #include <sys-uart.h>
+#include <sys-dram.h>
 #include <sys-sdcard.h>
 
 sunxi_serial_t uart_dbg = {
-        .base = SUNXI_UART0_BASE,
-        .id = 0,
-        .gpio_tx = {GPIO_PIN(GPIO_PORTE, 2), GPIO_PERIPH_MUX5},
-        .gpio_rx = {GPIO_PIN(GPIO_PORTE, 3), GPIO_PERIPH_MUX5},
+        .base = SUNXI_UART3_BASE,
+        .id = 3,
+        .gpio_tx = {GPIO_PIN(GPIO_PORTB, 6), GPIO_PERIPH_MUX7},
+        .gpio_rx = {GPIO_PIN(GPIO_PORTB, 7), GPIO_PERIPH_MUX7},
 };
 
 sunxi_spi_t sunxi_spi0 = {
@@ -50,6 +51,33 @@ sdhci_t sdhci0 = {
         .gpio_d1 = {GPIO_PIN(GPIO_PORTF, 0), GPIO_PERIPH_MUX2},
         .gpio_d2 = {GPIO_PIN(GPIO_PORTF, 5), GPIO_PERIPH_MUX2},
         .gpio_d3 = {GPIO_PIN(GPIO_PORTF, 4), GPIO_PERIPH_MUX2},
+};
+
+dram_para_t dram_para = {
+        .dram_clk = 792,
+        .dram_type = 3,
+        .dram_zq = 0x7b7bfb,
+        .dram_odt_en = 0x00,
+        .dram_para1 = 0x000010d2,
+        .dram_para2 = 0,
+        .dram_mr0 = 0x1c70,
+        .dram_mr1 = 0x42,
+        .dram_mr2 = 0x18,
+        .dram_mr3 = 0,
+        .dram_tpr0 = 0x004a2195,
+        .dram_tpr1 = 0x02423190,
+        .dram_tpr2 = 0x0008b061,
+        .dram_tpr3 = 0xb4787896,// unused
+        .dram_tpr4 = 0,
+        .dram_tpr5 = 0x48484848,
+        .dram_tpr6 = 0x00000048,
+        .dram_tpr7 = 0x1620121e,// unused
+        .dram_tpr8 = 0,
+        .dram_tpr9 = 0,// clock?
+        .dram_tpr10 = 0,
+        .dram_tpr11 = 0x00340000,
+        .dram_tpr12 = 0x00000046,
+        .dram_tpr13 = 0x34000100,
 };
 
 void clean_syterkit_data(void) {
