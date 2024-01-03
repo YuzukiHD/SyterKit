b2fa06e1be18e987ae0e8ba84dc2107ebe00aadb
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-01-03 18:52:01 +0800

    [bl33] add basic bl33 for syterkit

---
+++ bl33/source/timer.c
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: MIT */
+
+#include <byteorder.h>
+#include <endian.h>
+#include <io.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <types.h>
+
+// Stub function for handling signal raising
+int raise(int signum) {
+    return 0;// Always return 0 to indicate successful handling of the signal
+}
+
+// Inline function to retrieve the current value of the architecture-specific counter
+static inline uint64_t get_arch_counter(void) {
+    return 1; // fake func
+}
+
+// Delay function that waits for the specified number of microseconds
+static void udelay(uint32_t us) {
+    uint64_t t1, t2;
+
+    t1 = get_arch_counter();// Get the current value of the architecture-specific counter
+    t2 = t1 + us * 24;      // Calculate the target end time based on the counter frequency (24 MHz)
+    do {
+        t1 = get_arch_counter();// Continuously update the current time until it reaches the target end time
+    } while (t2 >= t1);
+}
+
+// Wrapper function for udelay with a different name
+void __usdelay(uint32_t loop) {
+    udelay(loop);// Call the udelay function to introduce a microsecond delay
+}
+
+void mdelay(uint32_t ms) {
+    udelay(ms * 1000);
+    uint32_t now;
+
+    now = time_ms();
+    while (time_ms() - now < ms) {
+    };
+}
+
+uint32_t time_ms(void) {
+    return get_arch_counter() / 24000;
+}
