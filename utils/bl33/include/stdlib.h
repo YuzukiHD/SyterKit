795ed31a9c3e87ee73c9c1f7fde42bb9e93a20ff
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-01-05 19:07:56 +0800

    [bl33] move bl33 to utils

---
+++ utils/bl33/include/stdlib.h
@@ -0,0 +1,26 @@
+#ifndef __STDLIB_H__
+#define __STDLIB_H__
+
+#include <types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void sunxi_uart_init(uint32_t base);
+
+void sunxi_uart_putc(char c);
+
+void uart_printf(const char *fmt, ...);
+
+void printf(const char *fmt, ...);
+
+uint32_t time_ms(void);
+
+void mdelay(uint32_t ms);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STDLIB_H__ */
