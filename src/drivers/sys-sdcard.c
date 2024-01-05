aaa754d7d3a5c9577b42f68603fbcadd8082e4b2
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-01-05 19:07:47 +0800

    [driver] dram use rtc to pass data

--- src/drivers/sys-sdcard.c
+++ src/drivers/sys-sdcard.c
@@ -577,8 +577,7 @@ static bool sdmmc_detect(sdhci_t *hci, sdmmc_t *card) {
         card->capacity = (csize + 1) << (cmult + 2);
     }
     card->capacity *= 1 << UNSTUFF_BITS(card->csd, 80, 4);
-    printk(LOG_LEVEL_INFO, "SMHC: capacity %.1fGB\n",
-           (f32) ((f64) card->capacity / (f64) 1000000000.0));
+    printk(LOG_LEVEL_INFO, "SMHC: capacity %.1fGB\n", (f32) ((f64) card->capacity / (f64) 1000000000.0));
 
     if (hci->isspi) {
         if (!sdhci_set_clock(hci, min(card->tran_speed, hci->clock)) ||
