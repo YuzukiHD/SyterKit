6367fb608b1069ff46e1fbd1113e44d5b857d731
柚木 鉉 <GloomyGhost@GloomyGhost.com>
2024-01-08 22:47:27 +0800

    Update main.c

--- board/yuzukilizard/syter_boot_spi/main.c
+++ board/yuzukilizard/syter_boot_spi/main.c
@@ -62,137 +62,12 @@ extern dram_para_t dram_para;
 
 extern sunxi_spi_t sunxi_spi0;
 
-extern sdhci_t sdhci0;
-
 image_info_t image;
 
 unsigned int code[9];
 unsigned int magic;
 unsigned int start;
 unsigned int end;
-
-static int boot_image_setup(unsigned char *addr, unsigned int *entry) {
-    linux_zimage_header_t *zimage_header = (linux_zimage_header_t *) addr;
-
-    printk(LOG_LEVEL_INFO, "Linux zImage->code  = 0x");
-    for (int i = 0; i < 9; i++)
-        printk(LOG_LEVEL_MUTE, "%x", code[i]);
-
-    printk(LOG_LEVEL_MUTE, "\n");
-    printk(LOG_LEVEL_DEBUG, "Linux zImage->magic = 0x%x\n",
-           zimage_header->magic);
-    printk(LOG_LEVEL_DEBUG, "Linux zImage->start = 0x%x\n",
-           (unsigned int) addr + zimage_header->start);
-    printk(LOG_LEVEL_DEBUG, "Linux zImage->end   = 0x%x\n",
-           (unsigned int) addr + zimage_header->end);
-
-    if (zimage_header->magic == LINUX_ZIMAGE_MAGIC) {
-        *entry = ((unsigned int) addr + zimage_header->start);
-        return 0;
-    }
-
-    printk(LOG_LEVEL_ERROR, "unsupported kernel image\n");
-
-    return -1;
-}
-
-#define CHUNK_SIZE 0x20000
-
-static int fatfs_loadimage(char *filename, BYTE *dest) {
-    FIL file;
-    UINT byte_to_read = CHUNK_SIZE;
-    UINT byte_read;
-    UINT total_read = 0;
-    FRESULT fret;
-    int ret;
-    uint32_t start, time;
-
-    fret = f_open(&file, filename, FA_OPEN_EXISTING | FA_READ);
-    if (fret != FR_OK) {
-        printk(LOG_LEVEL_ERROR,
-               "FATFS: open, filename: [%s]: error %d\n", filename,
-               fret);
-        ret = -1;
-        goto open_fail;
-    }
-
-    start = time_ms();
-
-    do {
-        byte_read = 0;
-        fret = f_read(&file, (void *) (dest), byte_to_read, &byte_read);
-        dest += byte_to_read;
-        total_read += byte_read;
-    } while (byte_read >= byte_to_read && fret == FR_OK);
-
-    time = time_ms() - start + 1;
-
-    if (fret != FR_OK) {
-        printk(LOG_LEVEL_ERROR, "FATFS: read: error %d\n", fret);
-        ret = -1;
-        goto read_fail;
-    }
-    ret = 0;
-
-read_fail:
-    fret = f_close(&file);
-
-    printk(LOG_LEVEL_DEBUG, "FATFS: read in %ums at %.2fMB/S\n", time,
-           (f32) (total_read / time) / 1024.0f);
-
-open_fail:
-    return ret;
-}
-
-static int load_sdcard(image_info_t *image) {
-    FATFS fs;
-    FRESULT fret;
-    int ret;
-    uint32_t start;
-
-    uint32_t test_time;
-    start = time_ms();
-    sdmmc_blk_read(&card0, (uint8_t *) (SDRAM_BASE), 0,
-                   CONFIG_SDMMC_SPEED_TEST_SIZE);
-    test_time = time_ms() - start;
-    printk(LOG_LEVEL_DEBUG, "SDMMC: speedtest %uKB in %ums at %uKB/S\n",
-           (CONFIG_SDMMC_SPEED_TEST_SIZE * 512) / 1024, test_time,
-           (CONFIG_SDMMC_SPEED_TEST_SIZE * 512) / test_time);
-
-    start = time_ms();
-
-    fret = f_mount(&fs, "", 1);
-    if (fret != FR_OK) {
-        printk(LOG_LEVEL_ERROR, "FATFS: mount error: %d\n", fret);
-        return -1;
-    } else {
-        printk(LOG_LEVEL_DEBUG, "FATFS: mount OK\n");
-    }
-
-    printk(LOG_LEVEL_INFO, "FATFS: read %s addr=%x\n", image->of_filename,
-           (unsigned int) image->of_dest);
-    ret = fatfs_loadimage(image->of_filename, image->of_dest);
-    if (ret)
-        return ret;
-
-    printk(LOG_LEVEL_INFO, "FATFS: read %s addr=%x\n", image->filename,
-           (unsigned int) image->dest);
-    ret = fatfs_loadimage(image->filename, image->dest);
-    if (ret)
-        return ret;
-
-    /* umount fs */
-    fret = f_mount(0, "", 0);
-    if (fret != FR_OK) {
-        printk(LOG_LEVEL_ERROR, "FATFS: unmount error %d\n", fret);
-        return -1;
-    } else {
-        printk(LOG_LEVEL_DEBUG, "FATFS: unmount OK\n");
-    }
-    printk(LOG_LEVEL_DEBUG, "FATFS: done in %ums\n", time_ms() - start);
-
-    return 0;
-}
 
 int load_spi_nand(sunxi_spi_t *spi, image_info_t *image) {
     linux_zimage_header_t *hdr;
@@ -245,8 +120,6 @@ int load_spi_nand(sunxi_spi_t *spi, image_info_t *image) {
     return 0;
 }
 
-
-
 int main(void) {
     sunxi_serial_init(&uart_dbg);
 
@@ -269,27 +142,6 @@ int main(void) {
     strcpy(image.filename, CONFIG_KERNEL_FILENAME);
     strcpy(image.of_filename, CONFIG_DTB_FILENAME);
 
-    if (sunxi_sdhci_init(&sdhci0) != 0) {
-        printk(LOG_LEVEL_ERROR, "SMHC: %s controller init failed\n",
-               sdhci0.name);
-    } else {
-        printk(LOG_LEVEL_INFO,
-               "SMHC: %s controller v%x initialized\n", sdhci0.name,
-               sdhci0.reg->vers);
-    }
-    if (sdmmc_init(&card0, &sdhci0) != 0) {
-        printk(LOG_LEVEL_WARNING, "SMHC: init failed, trying SPI\n");
-        goto _spi;
-    }
-
-    if (load_sdcard(&image) != 0) {
-        printk(LOG_LEVEL_WARNING,
-               "SMHC: loading failed, trying SPI\n");
-    } else {
-        goto _boot;
-    }
-
-_spi:
     dma_init();
     dma_test((uint32_t *) CONFIG_DTB_LOAD_ADDR,
              (uint32_t *) CONFIG_KERNEL_LOAD_ADDR);
@@ -305,7 +157,6 @@ _spi:
     sunxi_spi_disable(&sunxi_spi0);
     dma_exit();
 
-_boot:
     if (boot_image_setup((unsigned char *) image.dest, &entry_point)) {
         printk(LOG_LEVEL_ERROR, "boot setup failed\n");
         abort();
