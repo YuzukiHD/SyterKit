30143819c5e2b5cc908f16b4d910ea262a22d0eb
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-01-10 18:40:14 +0800

    [board] add dongshanpi-aict board

---
+++ board/dongshanpi-aict/i2c_oled/main.c
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <types.h>
+
+#include <log.h>
+
+#include <common.h>
+
+#include "sys-uart.h"
+
+#include "oled.h"
+
+extern sunxi_serial_t uart_dbg;
+
+int main(void) {
+    sunxi_serial_init(&uart_dbg);
+
+    sunxi_clk_init();
+
+    printk(LOG_LEVEL_INFO, "Hello World\n");
+
+    OLED_Init();
+
+    OLED_ShowString(12, 16, "SyterKit", 16, 1);
+	OLED_ShowString(20, 32, "I2C OLED", 16, 1);
+
+    OLED_Refresh();
+
+    abort();
+
+    return 0;
+}
