65a5f6e4639ddbce03e2aa4f384d29beb2a2e9f3
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-01-11 22:06:13 +0800

    [fix] timer count

---
+++ board/100ask-t113i/os_test/main.c
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <types.h>
+
+#include <log.h>
+#include <os.h>
+#include <timer.h>
+
+extern sunxi_serial_t uart_dbg;
+
+static void timer_500ms_cb(void *arg, uint32_t event) {
+    printk(LOG_LEVEL_INFO, "Timer 500ms callback\n");
+}
+
+static void timer_1500ms_run2_cb(void *arg, uint32_t event) {
+    printk(LOG_LEVEL_INFO, "Timer 500ms run 2 times callback\n");
+}
+
+int main(void) {
+    sunxi_serial_init(&uart_dbg);
+
+    sunxi_clk_init();
+
+    printk(LOG_LEVEL_INFO, "Hello World!\n");
+
+    timer_t timer_500ms;
+    timer_create(&timer_500ms, timer_500ms_cb, NULL);
+    timer_start(&timer_500ms, TIMER_ALWAYS_RUN, 500);
+
+    timer_t timer_1500ms_run2;
+    timer_create(&timer_1500ms_run2, timer_1500ms_run2_cb, NULL);
+    timer_start(&timer_1500ms_run2, 2, 1500);
+
+    while (1) {
+        timer_handle();
+        mdelay(1);
+    }
+
+    return 0;
+}
--- src/arch/arm32/timer.c
+++ src/arch/arm32/timer.c
@@ -45,20 +45,14 @@ uint64_t time_us(void) {
 }
 
 void udelay(uint64_t us) {
-    uint64_t now;
-
-    now = time_us();
-    while (time_us() - now < us) {
-    };
+    volatile uint32_t counter = us * 8;
+    sdelay(counter);
 }
 
 void mdelay(uint32_t ms) {
-    udelay(ms * 1000);
-    uint32_t now;
-
-    now = time_ms();
-    while (time_ms() - now < ms) {
-    };
+    while (ms--) {
+        udelay(1000);
+    }
 }
 
 void sdelay(uint32_t loops) {
