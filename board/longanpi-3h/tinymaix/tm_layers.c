5470416d77b0ed6a981890d6f98796f4463a3770
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-02-03 22:08:41 +0800

    [arch] god dam tinymaix

--- board/longanpi-3h/tinymaix/tm_layers.c
+++ board/longanpi-3h/tinymaix/tm_layers.c
@@ -10,8 +10,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 // It is default O0 implement
-#include "float.h"
-#include "math.h"
+#include "limits.h"
 #include "tinymaix.h"
 
 #if TM_OPT_LEVEL == TM_OPT0
