b7acafb427aa391581bfe15b7fde6492530a8891
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-02-04 20:28:10 +0800

    [image] delete some unused code

--- src/image/zimage.c
+++ src/image/zimage.c
@@ -10,12 +10,6 @@
 int zImage_loader(uint8_t *addr, uint32_t *entry) {
     linux_zimage_header_t *zimage_header = (linux_zimage_header_t *) addr;
 
-    printk(LOG_LEVEL_INFO, "Linux zImage->code  = 0x");
-    for (int i = 0; i < 9; i++) {
-        printk(LOG_LEVEL_MUTE, "%x", zimage_header->code[i]);
-    }
-
-    printk(LOG_LEVEL_MUTE, "\n");
     printk(LOG_LEVEL_DEBUG, "Linux zImage->magic = 0x%x\n", zimage_header->magic);
     printk(LOG_LEVEL_DEBUG, "Linux zImage->start = 0x%x\n", (uint32_t) addr + zimage_header->start);
     printk(LOG_LEVEL_DEBUG, "Linux zImage->end   = 0x%x\n", (uint32_t) addr + zimage_header->end);
