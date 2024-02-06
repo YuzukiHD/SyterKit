c9d75e2c441d20248ea75698f37e90e4b657e815
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-02-06 23:44:53 +0800

    [board] add hifi4 rproc for d1-h

--- include/drivers/sys-rproc.h
+++ include/drivers/sys-rproc.h
@@ -11,6 +11,8 @@ extern "C" {
     #include <sun8iw21/sys-rproc.h>
 #elif defined(CONFIG_CHIP_SUN8IW20)
     #include <sun8iw20/sys-rproc.h>
+#elif defined(CONFIG_CHIP_SUN20IW1)
+    #include <sun20iw1/sys-rproc.h>
 #else
     #error "Unsupported chip"
 #endif
