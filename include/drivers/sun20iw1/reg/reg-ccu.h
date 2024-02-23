9036482c7d783bf338fb4137fa0680335ab58d6d
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-02-24 00:26:16 +0800

    [board] D1-H Support HIFI4 load

--- include/drivers/sun20iw1/reg/reg-ccu.h
+++ include/drivers/sun20iw1/reg/reg-ccu.h
@@ -95,6 +95,25 @@
 #define CCU_MMC_BGR_SMHC1_RST (1 << 17)
 #define CCU_MMC_BGR_SMHC2_RST (1 << 18)
 
+/* DSP base */
+#define CCU_DSP_CLK_REG (0xc70)
+#define CCU_BIT_DSP_SCLK_GATING (31)
+#define CCU_DSP_CLK_M_MASK (0x1f << 0)
+#define CCU_DSP_CLK_SRC_MASK (0x7 << 24)
+#define CCU_DSP_CLK_SRC_HOSC (0)
+#define CCU_DSP_CLK_SRC_32K (0x1 << 24)
+#define CCU_DSP_CLK_SRC_16M (0x2 << 24)
+#define CCU_DSP_CLK_SRC_PERI2X (0x3 << 24)
+#define CCU_DSP_CLK_SRC_AUDIO1_DIV2 (0x4 << 24)
+/* x must be 1 - 32 */
+#define CCU_DSP_CLK_FACTOR_M(x) (((x)-1) << 0)
+
+#define CCU_DSP_BGR_REG (0xc7c)
+#define CCU_BIT_DSP0_CFG_GATING (1)
+#define CCU_BIT_DSP0_RST (16)
+#define CCU_BIT_DSP0_CFG_RST (17)
+#define CCU_BIT_DSP0_DBG_RST (18)
+
 /* RISC-V base */
 #define CCU_RISCV_GATING_RST_REG (CCU_BASE + 0x0d04)
 #define CCU_RISCV_CLK_GATING (0x1 << 31)
--- src/drivers/sun20iw1/sys-rproc.c
+++ src/drivers/sun20iw1/sys-rproc.c
@@ -13,40 +13,93 @@
 
 #include <sys-rproc.h>
 
-void sunxi_hifi4_clock_init(uint32_t addr) {
+static void sram_remap_set(int value) {
+    uint32_t val = 0;
 
+    val = readl(SUNXI_SYSCRL_BASE + SRAMC_SRAM_REMAP_REG);
+    val &= ~(1 << BIT_SRAM_REMAP_ENABLE);
+    val |= (value << BIT_SRAM_REMAP_ENABLE);
+    writel(val, SUNXI_SYSCRL_BASE + SRAMC_SRAM_REMAP_REG);
+}
+
+static void sunxi_hifi4_set_run_stall(uint32_t value) {
+    uint32_t reg_val = 0;
+
+    reg_val = readl(DSP0_CFG_BASE + DSP_CTRL_REG0);
+    reg_val &= ~(1 << BIT_RUN_STALL);
+    reg_val |= (value << BIT_RUN_STALL);
+    writel(reg_val, (DSP0_CFG_BASE + DSP_CTRL_REG0));
+}
+
+void sunxi_hifi4_clock_init(uint32_t addr) {
+    uint32_t reg_val = 0;
+
+    sram_remap_set(1);
+
+    reg_val |= CCU_DSP_CLK_SRC_PERI2X;
+    reg_val |= CCU_DSP_CLK_FACTOR_M(2);
+    reg_val |= (1 << CCU_BIT_DSP_SCLK_GATING);
+    writel(reg_val, CCU_BASE + CCU_DSP_CLK_REG);
+
+    /* clock gating */
+    reg_val = readl(CCU_BASE + CCU_DSP_BGR_REG);
+    reg_val |= (1 << CCU_BIT_DSP0_CFG_GATING);
+    writel(reg_val, CCU_BASE + CCU_DSP_BGR_REG);
+
+    /* reset */
+    reg_val = readl(CCU_BASE + CCU_DSP_BGR_REG);
+    reg_val |= (1 << CCU_BIT_DSP0_CFG_RST);
+    reg_val |= (1 << CCU_BIT_DSP0_DBG_RST);
+    writel(reg_val, CCU_BASE + CCU_DSP_BGR_REG);
+
+    /* set external Reset Vector if needed */
+    if (addr != DSP_DEFAULT_RST_VEC) {
+        writel(addr, DSP0_CFG_BASE + DSP_ALT_RESET_VEC_REG);
+
+        reg_val = readl(DSP0_CFG_BASE + DSP_CTRL_REG0);
+        reg_val |= (1 << BIT_START_VEC_SEL);
+        writel(reg_val, DSP0_CFG_BASE + DSP_CTRL_REG0);
+    }
+
+    /* set runstall */
+    sunxi_hifi4_set_run_stall(1);
+
+    /* set dsp clken */
+    reg_val = readl(DSP0_CFG_BASE + DSP_CTRL_REG0);
+    reg_val |= (1 << BIT_DSP_CLKEN);
+    writel(reg_val, DSP0_CFG_BASE + DSP_CTRL_REG0);
+
+    /* de-assert dsp0 */
+    reg_val = readl(CCU_BASE + CCU_DSP_BGR_REG);
+    reg_val |= (1 << CCU_BIT_DSP0_RST);
+    writel(reg_val, CCU_BASE + CCU_DSP_BGR_REG);
+
+/*
+    reg_val = readl(CCU_BASE + CCU_DSP_CLK_REG);
+    printk(LOG_LEVEL_INFO, "CCU_DSP_CLK_REG = %x\n", reg_val);
+    reg_val = readl(CCU_BASE + CCU_DSP_BGR_REG);
+    printk(LOG_LEVEL_INFO, "CCU_DSP_BGR_REG = %x\n", reg_val);
+    reg_val = readl(DSP0_CFG_BASE + DSP_CTRL_REG0);
+    printk(LOG_LEVEL_INFO, "DSP_CTRL_REG0 = %x\n", reg_val);
+*/
+}
+
+void sunxi_hifi4_start(void) {
+    /* set dsp use local ram */
+    sram_remap_set(0);
+
+    /* clear runstall */
+    sunxi_hifi4_set_run_stall(0);
 }
 
 void sunxi_hifi4_clock_reset(void) {
+    uint32_t reg_val;
 
-}
+    /* assert */
+    reg_val = readl(CCU_BASE + CCU_DSP_BGR_REG);
+    reg_val &= ~(1 << CCU_BIT_DSP0_CFG_GATING);
+    writel(reg_val, CCU_BASE + CCU_DSP_BGR_REG);
 
-void dump_hifi4_clock(void) {
-    uint32_t reg_val, pll_perf, factor_m, factor_n, pll_riscv;
-    uint32_t plln, pllm;
-    uint8_t p0, p1;
-
-    /* PLL PERI0 */
-    reg_val = read32(CCU_BASE + CCU_PLL_PERI0_CTRL_REG);
-
-    if (reg_val & (1 << 31)) {
-        plln = ((reg_val >> 8) & 0xff) + 1;
-        pllm = (reg_val & 0x01) + 1;
-        p0 = ((reg_val >> 16) & 0x03) + 1;
-        p1 = ((reg_val >> 20) & 0x03) + 1;
-
-        pll_perf = (24 * plln) / (pllm * p1);
-    } else {
-        printk(LOG_LEVEL_INFO, "CLK: PLL_peri disabled\n");
-        return;
-    }
-
-    reg_val = read32(CCU_RISCV_CLK_REG);
-    factor_m = (reg_val & 0x1F) + 1;
-    factor_n = ((reg_val >> 8) & 0x3) + 1;
-    pll_riscv = pll_perf / factor_m;
-
-    printk(LOG_LEVEL_INFO, "CLK: HIFI4 PLL FREQ=%uMHz\n", pll_riscv);
-    printk(LOG_LEVEL_INFO, "CLK: HIFI4 AXI FREQ=%uMHz\n",
-           pll_riscv / factor_n);
+    reg_val = 0;
+    writel(reg_val, CCU_BASE + CCU_DSP_BGR_REG);
 }
