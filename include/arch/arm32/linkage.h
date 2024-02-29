e70cf74794a2d9d06b815880f5bdc98f0eadccef
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-02-29 19:34:13 +0800

    [arch] fix armv7-a vector table and add stack, move mode to define

--- include/arch/arm32/linkage.h
+++ include/arch/arm32/linkage.h
@@ -25,6 +25,18 @@ extern "C" {
     .type name, % function; \
     END(name)
 
+#define  ARMV7_USR_MODE        0x10
+#define  ARMV7_FIQ_MODE        0x11
+#define  ARMV7_IRQ_MODE        0x12
+#define  ARMV7_SVC_MODE        0x13
+#define  ARMV7_MON_MODE        0x16
+#define  ARMV7_ABT_MODE        0x17
+#define  ARMV7_UND_MODE        0x1b
+#define  ARMV7_SYSTEM_MODE     0x1f
+#define  ARMV7_MODE_MASK       0x1f
+#define  ARMV7_FIQ_MASK        0x40
+#define  ARMV7_IRQ_MASK        0x80
+
 #ifdef __cplusplus
 }
 #endif
