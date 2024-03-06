fa9095e00e4cfe80f617ea04f2d7fc13e3db378c
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-03-07 07:46:22 +0800

    [test] wip dram init binary

--- board/lt527x/init_dram/main.c
+++ board/lt527x/init_dram/main.c
@@ -2,8 +2,8 @@
 
 #include <stdbool.h>
 #include <stddef.h>
-#include <types.h>
 #include <stdint.h>
+#include <types.h>
 
 #include <log.h>
 
@@ -20,7 +20,7 @@ extern sunxi_i2c_t i2c_pmu;
 extern void set_rpio_power_mode(void);
 extern void rtc_set_vccio_det_spare(void);
 
-static void set_axp323_pmu_fin_voltage(char* power_name, uint32_t voltage){
+static void set_axp323_pmu_fin_voltage(char *power_name, uint32_t voltage) {
     int set_vol = voltage;
     int temp_vol, src_vol = pmu_axp1530_get_vol(&i2c_pmu, power_name);
     if (src_vol > voltage) {
@@ -35,7 +35,7 @@ static void set_axp323_pmu_fin_voltage(char* power_name, uint32_t voltage){
     mdelay(30); /* Delay 300ms for pmu bootup */
 }
 
-static void set_axp717_pmu_fin_voltage(char* power_name, uint32_t voltage){
+static void set_axp717_pmu_fin_voltage(char *power_name, uint32_t voltage) {
     int set_vol = voltage;
     int temp_vol, src_vol = pmu_axp2202_get_vol(&i2c_pmu, power_name);
     if (src_vol > voltage) {
@@ -63,7 +63,7 @@ int main(void) {
     sunxi_clk_dump();
 
     rtc_set_vccio_det_spare();
-    
+
     set_rpio_power_mode();
 
     sunxi_i2c_init(&i2c_pmu);
@@ -77,6 +77,8 @@ int main(void) {
     pmu_axp1530_dump(&i2c_pmu);
 
     pmu_axp2202_dump(&i2c_pmu);
+
+    enable_sram_a3();
 
     printk(LOG_LEVEL_INFO, "DRAM: DRAM Size = %dMB\n", sunxi_dram_init(NULL));
 
