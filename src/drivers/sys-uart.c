07d6bc5a3b65c6d8e6908f7ac0f36557049c992a
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-03-19 09:53:10 +0800

    [driver] new serial driver reformat

--- src/drivers/sys-uart.c
+++ src/drivers/sys-uart.c
@@ -13,6 +13,7 @@
 #include <sys-clk.h>
 
 #define CCU_UART_RST_OFFSET (16)
+#define SERIAL_PARENT_CLK (24000000)
 
 static void sunxi_serial_clock_init(sunxi_serial_t *uart) {
     uint32_t addr;
@@ -51,14 +52,44 @@ void sunxi_serial_init(sunxi_serial_t *uart) {
         uart->parity = UART_PARITY_NO;
     }
 
+    /* Typecast to sunxi_serial_reg_t structure pointer */
     sunxi_serial_reg_t *serial_reg = (sunxi_serial_reg_t *) uart->base;
+
+    /* Set control register MCR to 0x3 */
     serial_reg->mcr = 0x3;
-    uint32_t uart_clk = (24000000 + 8 * uart->baud_rate) / (16 * uart->baud_rate);
+
+    /* Calculate UART clock frequency */
+    uint32_t uart_clk = (SERIAL_PARENT_CLK + 8 * uart->baud_rate) / (16 * uart->baud_rate);
+
+    /* Set bit 7 of line control register LCR */
     serial_reg->lcr |= 0x80;
+
+    /* Set divisor latch high register DLH */
     serial_reg->dlh = uart_clk >> 8;
+
+    /* Set divisor latch low register DLL */
     serial_reg->dll = uart_clk & 0xff;
+
+    /* Clear bit 7 of line control register LCR */
     serial_reg->lcr &= ~0x80;
-    serial_reg->lcr = ((uart->parity & 0x03) << 3) | ((uart->stop & 0x01) << 2) | (uart->dlen & 0x03);
+
+    /* Set parity, stop bits, and data length in line control register LCR */
+    /* Set parity bits based on uart->parity value */
+    serial_reg->lcr |= (uart->parity & 0x03) << 3;
+
+    /* Set stop bits based on uart->stop value */
+    serial_reg->lcr |= (uart->stop & 0x01) << 2;
+
+    /* Set data length based on uart->dlen value */
+    serial_reg->lcr |= uart->dlen & 0x03;
+
+    /* Configure FIFO Control Register (FCR) */
+    /* Bit 0: FIFO Enable (1 - Enable FIFO) */
+    /* Bit 1: RCVR Reset (1 - Clear Receive FIFO) */
+    /* Bit 2: XMIT Reset (1 - Clear Transmit FIFO) */
+    /* Bit 3: DMA Mode Select (0 - Disable DMA Mode) */
+    /* Bit 4: Reserved (0) */
+    /* Bits 5-7: Trigger Level (011 - Trigger Level of 8 bytes) */
     serial_reg->fcr = 0x7;
 
     /* Config uart TXD and RXD pins */
@@ -79,9 +110,9 @@ char sunxi_serial_getc(void *arg) {
     sunxi_serial_t *uart = (sunxi_serial_t *) arg;
     sunxi_serial_reg_t *serial_reg = (sunxi_serial_reg_t *) uart->base;
 
-	while ((serial_reg->lsr & 1) == 0)
-		;
-	return serial_reg->rbr;
+    while ((serial_reg->lsr & 1) == 0)
+        ;
+    return serial_reg->rbr;
 }
 
 int sunxi_serial_tstc(void *arg) {
