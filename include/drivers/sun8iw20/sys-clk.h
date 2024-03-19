dcde8ef24ffacba58e5f2c87e3786bece01ace51
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-03-19 14:29:08 +0800

    [driver] update clk driver

--- include/drivers/sun8iw20/sys-clk.h
+++ include/drivers/sun8iw20/sys-clk.h
@@ -3,10 +3,4 @@
 
 #include "reg/reg-ccu.h"
 
-void sunxi_clk_init(void);
-
-void sunxi_clk_reset(void);
-
-void sunxi_clk_dump(void);
-
 #endif// __SUN8IW20_CLK_H__
--- include/drivers/sys-clk.h
+++ include/drivers/sys-clk.h
@@ -1,6 +1,13 @@
 #ifndef __SYS_CLK_H__
 #define __SYS_CLK_H__
 
+#include <io.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <types.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif// __cplusplus
@@ -22,6 +29,46 @@ extern "C" {
 #else
     #error "Unsupported chip"
 #endif
+
+/**
+ * @brief Initialize the global clocks.
+ *
+ * This function initializes the global clocks, including PLLs and clock dividers.
+ */
+void sunxi_clk_init(void);
+
+/**
+ * @brief Reset the global clocks.
+ *
+ * This function resets all global clocks to their default values.
+ */
+void sunxi_clk_reset(void);
+
+/**
+ * @brief Dump all clock-related register values.
+ *
+ * This function prints out all clock-related register values for debugging and observation.
+ */
+void sunxi_clk_dump(void);
+
+/**
+ * @brief Get the clock rate of the PERI1X bus.
+ *
+ * @return The clock rate of the PERI1X bus in Hz.
+ */
+uint32_t sunxi_clk_get_peri1x_rate();
+
+/**
+ * @brief Get the clock rate of the PERI1X bus.
+ *
+ * @return The clock rate of the PERI1X bus in Hz.
+ */
+uint32_t sunxi_clk_get_peri1x_rate();
+
+/**
+ * @brief Deinitialize USB clock.
+ */
+void sunxi_usb_clk_deinit();
 
 #ifdef __cplusplus
 }
