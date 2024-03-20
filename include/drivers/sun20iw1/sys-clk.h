bba703de252e7eeb27c4c6a7116918851caedb6d
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-03-20 14:27:44 +0800

    [driver] Unified DRAM driver

--- include/drivers/sun20iw1/sys-clk.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SUN20IW1_CLK_H__
-#define __SUN20IW1_CLK_H__
-
-#include "reg/reg-ccu.h"
-
-#endif// __SUN20IW1_CLK_H__
--- src/drivers/sun20iw1/sys-dram.c
+++ src/drivers/sun20iw1/sys-dram.c
@@ -1267,7 +1267,7 @@ static int auto_scan_dram_config(dram_para_t *para) {
     return 1;
 }
 
-int init_DRAM(int type, dram_para_t *para) {
+static int init_DRAM(int type, dram_para_t *para) {
     uint32_t rc, mem_size_mb;
 
     printk(LOG_LEVEL_DEBUG, "DRAM BOOT DRIVE INFO: %s\n", "V0.24");
@@ -1380,6 +1380,7 @@ int init_DRAM(int type, dram_para_t *para) {
     return mem_size_mb;
 }
 
-uint64_t sunxi_dram_init(dram_para_t *para) {
-    return init_DRAM(0, para);
+uint64_t sunxi_dram_init(void *para) {
+    dram_para_t *dram_para = (dram_para_t *) para;
+    return init_DRAM(0, dram_para);
 };
