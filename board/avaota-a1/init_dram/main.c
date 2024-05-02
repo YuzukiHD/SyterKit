faa4f05a2975240dbe478494e8c66086f03f205f
sky <code@sky.com>
2024-05-02 08:26:42 +0800

    [board] avaota support soft timing training

--- board/avaota-a1/init_dram/main.c
+++ board/avaota-a1/init_dram/main.c
@@ -21,74 +21,36 @@ extern sunxi_i2c_t i2c_pmu;
 extern void set_rpio_power_mode(void);
 extern void rtc_set_vccio_det_spare(void);
 
-static void set_axp323_pmu_fin_voltage(char *power_name, uint32_t voltage) {
-    int set_vol = voltage;
-    int temp_vol, src_vol = pmu_axp1530_get_vol(&i2c_pmu, power_name);
-    if (src_vol > voltage) {
-        for (temp_vol = src_vol; temp_vol >= voltage; temp_vol -= 50) {
-            pmu_axp1530_set_vol(&i2c_pmu, power_name, temp_vol, 1);
-        }
-    } else if (src_vol < voltage) {
-        for (temp_vol = src_vol; temp_vol <= voltage; temp_vol += 50) {
-            pmu_axp1530_set_vol(&i2c_pmu, power_name, temp_vol, 1);
-        }
-    }
-    mdelay(30); /* Delay 300ms for pmu bootup */
-}
-
-static void set_axp717_pmu_fin_voltage(char *power_name, uint32_t voltage) {
-    int set_vol = voltage;
-    int temp_vol, src_vol = pmu_axp2202_get_vol(&i2c_pmu, power_name);
-    if (src_vol > voltage) {
-        for (temp_vol = src_vol; temp_vol >= voltage; temp_vol -= 50) {
-            pmu_axp2202_set_vol(&i2c_pmu, power_name, temp_vol, 1);
-        }
-    } else if (src_vol < voltage) {
-        for (temp_vol = src_vol; temp_vol <= voltage; temp_vol += 50) {
-            pmu_axp2202_set_vol(&i2c_pmu, power_name, temp_vol, 1);
-        }
-    }
-    mdelay(30); /* Delay 300ms for pmu bootup */
-}
-
-void arm32_do_prefetch_abort(struct arm_regs_t *regs) {
-    main();
-}
-
-static int boot_dram_init_status = 0;
-
 int main(void) {
-    if (boot_dram_init_status == 0) {
-        sunxi_serial_init(&uart_dbg);
+    sunxi_serial_init(&uart_dbg);
 
-        show_banner();
+    show_banner();
 
-        sunxi_clk_init();
+    sunxi_clk_init();
 
-        sunxi_clk_dump();
+    sunxi_clk_dump();
 
-        rtc_set_vccio_det_spare();
+    rtc_set_vccio_det_spare();
 
-        set_rpio_power_mode();
+    set_rpio_power_mode();
 
-        sunxi_i2c_init(&i2c_pmu);
+    sunxi_i2c_init(&i2c_pmu);
 
-        pmu_axp1530_init(&i2c_pmu);
+    pmu_axp1530_init(&i2c_pmu);
 
-        pmu_axp2202_init(&i2c_pmu);
+    pmu_axp2202_init(&i2c_pmu);
 
-        mdelay(30); /* Delay 300ms for pmu bootup */
+    pmu_axp2202_set_vol(&i2c_pmu, "dcdc3", 1160, 1);
 
-        pmu_axp1530_dump(&i2c_pmu);
+    mdelay(30); /* Delay 300ms for pmu bootup */
 
-        pmu_axp2202_dump(&i2c_pmu);
+    pmu_axp1530_dump(&i2c_pmu);
 
-        enable_sram_a3();
+    pmu_axp2202_dump(&i2c_pmu);
 
-        boot_dram_init_status = 1;
+    enable_sram_a3();
 
-        printk_info("DRAM: DRAM Size = %dMB\n", sunxi_dram_init(NULL));
-    }
+    printk_info("DRAM: DRAM Size = %dMB\n", sunxi_dram_init(NULL));
 
     sunxi_clk_dump();
 
