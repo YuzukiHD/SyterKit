9fc1afdf48d33a898f4e401f18f5e09029650f7e
SamulKyull <SamulKyull@SamulKyull.xyz>
2024-06-02 21:51:08 +0800

    [driver] back port to mmc v1

---
+++ src/drivers/mmc/sys-mmc.c
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier:	GPL-2.0+ */
+
+#include <barrier.h>
+#include <io.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <types.h>
+
+#include <log.h>
+#include <timer.h>
+
+#include <sys-clk.h>
+#include <sys-gpio.h>
+
+#include <mmc/sys-mmc.h>
+#include <mmc/sys-sdhci.h>
+
+static int sunxi_mmc_send_status() {
+}
