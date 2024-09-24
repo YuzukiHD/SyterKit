42b2eddf984308aab3527207d7a24c2cf33bf0d1
DongQing <placebo27@hust.edu.cn>
2024-09-24 21:29:59 +0800

    [board] add macro module for 100ask-d1-h Rust support
    Co-authored-by: Zhouqi Jiang <luojia@hust.edu.cn>
    Signed-off-by: DongQing <placebo27@hust.edu.cn>

---
+++ board/100ask-d1-h-rs/macros/src/lib.rs
@@ -0,0 +1,101 @@
+use proc_macro2::Span;
+use quote::quote;
+use syn::{
+    parse, parse_macro_input, spanned::Spanned, FnArg, ItemFn, ReturnType, Type, Visibility,
+};
+
+use proc_macro::TokenStream;
+
+/// ROM stage function entry.
+#[proc_macro_attribute]
+pub fn entry(args: TokenStream, input: TokenStream) -> TokenStream {
+    let f = parse_macro_input!(input as ItemFn);
+
+    // check the function arguments
+    if f.sig.inputs.len() != 2 {
+        return parse::Error::new(
+            f.sig.inputs.last().unwrap().span(),
+            "`#[entry]` function should include exactly two parameters",
+        )
+        .to_compile_error()
+        .into();
+    }
+
+    for arg in &f.sig.inputs {
+        match arg {
+            FnArg::Receiver(_) => {
+                return parse::Error::new(arg.span(), "invalid argument")
+                    .to_compile_error()
+                    .into();
+            }
+            FnArg::Typed(t) => {
+                if let Type::Path(_p) = &*t.ty {
+                    // empty
+                } else {
+                    return parse::Error::new(t.ty.span(), "argument type must be a path")
+                        .to_compile_error()
+                        .into();
+                }
+            }
+        }
+    }
+
+    // check the function signature
+    let valid_signature = f.sig.constness.is_none()
+        && f.sig.asyncness.is_none()
+        && f.vis == Visibility::Inherited
+        && f.sig.abi.is_none()
+        && f.sig.generics.params.is_empty()
+        && f.sig.generics.where_clause.is_none()
+        && f.sig.variadic.is_none()
+        && match f.sig.output {
+            ReturnType::Default => true,
+            _ => false,
+        };
+
+    if !valid_signature {
+        return parse::Error::new(
+            f.span(),
+            "`#[entry]` function must have signature `[unsafe] fn(p: Peripherals, c: Clocks)`",
+        )
+        .to_compile_error()
+        .into();
+    }
+
+    if !args.is_empty() {
+        return parse::Error::new(Span::call_site(), "This attribute accepts no arguments")
+            .to_compile_error()
+            .into();
+    }
+
+    let attrs = f.attrs;
+    let unsafety = f.sig.unsafety;
+    let args = f.sig.inputs;
+    let stmts = f.block.stmts;
+    let ret = f.sig.output;
+
+    quote!(
+        #[export_name = "main"]
+        pub fn main() {
+            let (p, c) = ::allwinner_rt::__rom_init_params();
+            let (__p, __uart0, __tx, __rx) = ::syterkit_100ask_d1_h::Peripherals::configure_uart0(p);
+            unsafe { __syterkit_macros__main(__p, c, __uart0, __tx, __rx) }
+        }
+        #[allow(non_snake_case)]
+        #[inline]
+        #(#attrs)*
+        #unsafety fn __syterkit_macros__main(
+            #args,
+            __uart0: ::allwinner_rt::soc::d1::UART0,
+            __tx: ::allwinner_hal::gpio::Function<'static, 'B', 8, 6>,
+            __rx: ::allwinner_hal::gpio::Function<'static, 'B', 9, 6>
+        ) #ret {
+            let mut __serial = ::allwinner_hal::uart::Serial::new(__uart0, (__tx, __rx), ::allwinner_hal::uart::Config::default(), &c, &p.ccu);
+            unsafe {
+                *::syterkit_100ask_d1_h::CONSOLE.lock() = Some(::syterkit_100ask_d1_h::SyterKitConsole { inner: __serial })
+            };
+            #(#stmts)*
+        }
+    )
+    .into()
+}
---
+++ board/100ask-d1-h-rs/src/bin/led-lightup.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use embedded_hal::digital::{InputPin, OutputPin};
+use panic_halt as _;
+use syterkit_100ask_d1_h::{entry, Clocks, Peripherals};
+
+#[entry]
+fn main(p: Peripherals, c: Clocks) {
+    // light up led
+    let mut pb5 = p.gpio.pb5.into_output();
+    pb5.set_high().unwrap();
+    let mut pc1 = p.gpio.pc1.into_output();
+    pc1.set_high().unwrap();
+
+    let mut pb0 = p.gpio.pb7.into_input();
+
+    pb0.with_output(|pad| pad.set_high()).unwrap();
+
+    let _input_high = pb0.is_high();
+}
