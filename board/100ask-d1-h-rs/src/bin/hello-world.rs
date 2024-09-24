2e75d63bfc0672f4d0d10a26c208a6b7bb918491
DongQing <placebo27@hust.edu.cn>
2024-09-24 21:29:59 +0800

    [board] add macro module for 100ask-d1-h Rust support
    Co-authored-by: Zhouqi Jiang <luojia@hust.edu.cn>
    Signed-off-by: DongQing <placebo27@hust.edu.cn>

--- board/100ask-d1-h-rs/src/bin/hello-world.rs
+++ board/100ask-d1-h-rs/src/bin/hello-world.rs
@@ -1,28 +1,10 @@
 #![no_std]
 #![no_main]
-use allwinner_hal::uart::{Config, Serial};
-use allwinner_rt::{entry, Clocks, Peripherals};
-use embedded_hal::digital::{InputPin, OutputPin};
-use embedded_io::Write;
+
 use panic_halt as _;
+use syterkit_100ask_d1_h::{entry, println, Clocks, Peripherals};
 
 #[entry]
 fn main(p: Peripherals, c: Clocks) {
-    // light up led
-    let mut pb5 = p.gpio.pb5.into_output();
-    pb5.set_high().unwrap();
-    let mut pc1 = p.gpio.pc1.into_output();
-    pc1.set_high().unwrap();
-
-    let mut pb0 = p.gpio.pb7.into_input();
-
-    pb0.with_output(|pad| pad.set_high()).unwrap();
-
-    let _input_high = pb0.is_high();
-
-    let tx = p.gpio.pb8.into_function::<6>();
-    let rx = p.gpio.pb9.into_function::<6>();
-    let mut serial = Serial::new(p.uart0, (tx, rx), Config::default(), &c, &p.ccu);
-
-    writeln!(serial, "Hello World!").unwrap();
+    println!("Hello World!");
 }
--- board/100ask-d1-h-rs/src/bin/init-dram.rs
+++ board/100ask-d1-h-rs/src/bin/init-dram.rs
@@ -1,18 +1,11 @@
 #![no_std]
 #![no_main]
-use allwinner_hal::uart::{Config, Serial};
-use allwinner_rt::{entry, Clocks, Peripherals};
-use embedded_io::Write;
 use panic_halt as _;
-use syterkit_100ask_d1_h::mctl;
+use syterkit_100ask_d1_h::{mctl, println, entry, Clocks, Peripherals};
 
 #[entry]
 fn main(p: Peripherals, c: Clocks) {
-    let tx = p.gpio.pb8.into_function::<6>();
-    let rx = p.gpio.pb9.into_function::<6>();
-    let mut serial = Serial::new(p.uart0, (tx, rx), Config::default(), &c, &p.ccu);
-
-    writeln!(serial, "DDR init").unwrap();
+    println!("DDR Init");
     let ram_size = mctl::init();
-    writeln!(serial, "{}M 🐏", ram_size).unwrap();
+    println!("{}M 🐏", ram_size);
 }
