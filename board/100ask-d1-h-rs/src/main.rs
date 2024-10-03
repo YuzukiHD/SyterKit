dbdc27cd4cf9e1ca57a4dc97f311665c758ee078
Zhouqi Jiang <luojia@hust.edu.cn>
2024-10-03 21:57:17 +0800

    [board] add cli support for 100ask-d1-h board
    crate embedded-cli is used, feature autocomplete and history are disabled
    
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- board/100ask-d1-h-rs/src/main.rs
+++ board/100ask-d1-h-rs/src/main.rs
@@ -1,8 +1,27 @@
 #![no_std]
 #![no_main]
 
+use core::convert::Infallible;
+
+use embedded_cli::{
+    cli::{CliBuilder, CliHandle},
+    Command,
+};
+use embedded_io::Read;
 use panic_halt as _;
-use syterkit::{clock_dump, entry, println, show_banner, Clocks, Peripherals};
+use syterkit::{clock_dump, entry, println, show_banner, Clocks, Peripherals, Stdout};
+
+#[derive(Command)]
+enum Base {
+    /// Get/set bootargs for kernel.
+    Bootargs,
+    /// Rescan TF Card and reload DTB, Kernel zImage.
+    Reload,
+    /// Print out env config.
+    Print,
+    /// Boot to Linux.
+    Boot,
+}
 
 #[entry] // This macro would initialize system clocks.
 fn main(p: Peripherals, _c: Clocks) {
@@ -15,4 +34,47 @@ fn main(p: Peripherals, _c: Clocks) {
 
     // Dump information about the system clocks.
     clock_dump(&p.ccu);
+
+    // Start boot command line.
+    let (command_buffer, history_buffer) = ([0; 32], [0; 32]);
+    let mut cli = CliBuilder::default()
+        .writer(syterkit::stdout())
+        .command_buffer(command_buffer)
+        .history_buffer(history_buffer)
+        .prompt("SyterKit> ")
+        .build()
+        .unwrap();
+    let mut stdin = syterkit::stdin();
+    loop {
+        let mut slice = [0];
+        stdin.read(&mut slice).ok();
+        let _ = cli.process_byte::<Base, _>(
+            slice[0],
+            &mut Base::processor(|cli, command| {
+                match command {
+                    Base::Bootargs => command_bootargs(cli),
+                    Base::Reload => command_reload(cli),
+                    Base::Boot => command_boot(cli),
+                    Base::Print => command_print(cli),
+                }
+                Ok(())
+            }),
+        );
+    }
+}
+
+fn command_bootargs<'a>(cli: &mut CliHandle<'a, Stdout, Infallible>) {
+    ufmt::uwrite!(cli.writer(), "TODO Bootargs").ok();
+}
+
+fn command_reload<'a>(cli: &mut CliHandle<'a, Stdout, Infallible>) {
+    ufmt::uwrite!(cli.writer(), "TODO Reload").ok();
+}
+
+fn command_boot<'a>(cli: &mut CliHandle<'a, Stdout, Infallible>) {
+    ufmt::uwrite!(cli.writer(), "TODO Boot").ok();
+}
+
+fn command_print<'a>(cli: &mut CliHandle<'a, Stdout, Infallible>) {
+    ufmt::uwrite!(cli.writer(), "TODO Print").ok();
 }
