ae664a916da174bc03a117528804ca32c3451028
Zhouqi Jiang <luojia@hust.edu.cn>
2024-10-05 14:48:34 +0800

    [board] read32 and write32 commands
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- board/100ask-d1-h-rs/src/main.rs
+++ board/100ask-d1-h-rs/src/main.rs
@@ -12,7 +12,7 @@ use panic_halt as _;
 use syterkit::{clock_dump, entry, println, show_banner, Clocks, Peripherals, Stdout};
 
 #[derive(Command)]
-enum Base {
+enum Base<'a> {
     /// Get/set bootargs for kernel.
     Bootargs,
     /// Rescan TF Card and reload DTB, Kernel zImage.
@@ -21,6 +21,19 @@ enum Base {
     Print,
     /// Boot to Linux.
     Boot,
+    // TODO: parse command name into 'read32', other than 'read-32'
+    /// Read 32-bit value from device register or memory.
+    Read32 {
+        /// The physical address this command would read.
+        address: &'a str,
+    },
+    /// Write 32-bit value to device register or memory.
+    Write32 {
+        /// The physical address this command would write into.
+        address: &'a str,
+        /// The 32-bit data this command would use.
+        data: &'a str,
+    },
 }
 
 #[entry] // This macro would initialize system clocks.
@@ -56,6 +69,8 @@ fn main(p: Peripherals, _c: Clocks) {
                     Base::Reload => command_reload(cli),
                     Base::Boot => command_boot(cli),
                     Base::Print => command_print(cli),
+                    Base::Read32 { address } => command_read32(cli, address),
+                    Base::Write32 { address, data } => command_write32(cli, address, data),
                 }
                 Ok(())
             }),
@@ -77,4 +92,47 @@ fn command_boot<'a>(cli: &mut CliHandle<'a, Stdout, Infallible>) {
 
 fn command_print<'a>(cli: &mut CliHandle<'a, Stdout, Infallible>) {
     ufmt::uwrite!(cli.writer(), "TODO Print").ok();
+}
+
+fn command_read32<'a, 'b>(_cli: &mut CliHandle<'a, Stdout, Infallible>, address: &'b str) {
+    let address: usize = match parse_value(address.trim()) {
+        Some(address) => address,
+        None => {
+            println!("error: invalid address, shoule be hexadecimal like 0x40000000, or decimal like 1073741824");
+            return;
+        }
+    };
+    let result = unsafe { core::ptr::read_volatile(address as *const u32) };
+    println!("Value at address 0x{:x}: 0x{:08x}.", address, result);
+}
+
+fn command_write32<'a, 'b>(
+    _cli: &mut CliHandle<'a, Stdout, Infallible>,
+    address: &'b str,
+    data: &'b str,
+) {
+    let address: usize = match parse_value(address.trim()) {
+        Some(address) => address,
+        None => {
+            println!("error: invalid address, shoule be hexadecimal like 0x40000000, or decimal like 1073741824");
+            return;
+        }
+    };
+    let data: u32 = match parse_value(data.trim()) {
+        Some(address) => address,
+        None => {
+            println!("error: invalid data, shoule be hexadecimal like 0x40000000, or decimal like 1073741824");
+            return;
+        }
+    };
+    unsafe { core::ptr::write_volatile(address as *mut u32, data) };
+    println!("Wrote 0x{:08x} to address 0x{:x}.", data, address);
+}
+
+fn parse_value<T: core::str::FromStr + num_traits::Num>(value: &str) -> Option<T> {
+    if value.starts_with("0x") {
+        T::from_str_radix(value.strip_prefix("0x").unwrap(), 16).ok()
+    } else {
+        value.parse::<T>().ok()
+    }
 }
