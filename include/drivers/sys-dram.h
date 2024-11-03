de37ff22c39cc1736eccdf115d5377b0ff9a2304
SamulKyull <SamulKyull@SamulKyull.xyz>
2024-11-03 10:39:18 +0800

    [board] add 100ask-ros support new clk

--- include/drivers/sys-dram.h
+++ include/drivers/sys-dram.h
@@ -28,6 +28,42 @@ enum sunxi_dram_type {
     SUNXI_DRAM_TYPE_LPDDR3 = 7,
 };
 
+#if defined SUNXI_DRAM_PARAM_V2
+typedef struct {
+    uint32_t dram_clk;
+    uint32_t dram_type;
+    uint32_t dram_dx_odt;
+    uint32_t dram_dx_dri;
+    uint32_t dram_ca_dri;
+    uint32_t dram_para0;
+    uint32_t dram_para1;
+    uint32_t dram_para2;
+    uint32_t dram_mr0;
+    uint32_t dram_mr1;
+    uint32_t dram_mr2;
+    uint32_t dram_mr3;
+    uint32_t dram_mr4;
+    uint32_t dram_mr5;
+    uint32_t dram_mr6;
+    uint32_t dram_mr11;
+    uint32_t dram_mr12;
+    uint32_t dram_mr13;
+    uint32_t dram_mr14;
+    uint32_t dram_mr16;
+    uint32_t dram_mr17;
+    uint32_t dram_mr22;
+    uint32_t dram_tpr0;
+    uint32_t dram_tpr1;
+    uint32_t dram_tpr2;
+    uint32_t dram_tpr3;
+    uint32_t dram_tpr6;
+    uint32_t dram_tpr10;
+    uint32_t dram_tpr11;
+    uint32_t dram_tpr12;
+    uint32_t dram_tpr13;
+    uint32_t dram_tpr14;
+} dram_para_t;
+#else
 typedef struct {
     // normal configuration
     uint32_t dram_clk;
@@ -60,6 +96,7 @@ typedef struct {
     uint32_t dram_tpr12;
     uint32_t dram_tpr13;
 } dram_para_t;
+#endif
 
 /**
  * @brief Get the size of the DRAM (Dynamic Random Access Memory).
