b72828a06de1a8695d8e14bb143e796c86fde640
SamulKyull <SamulKyull@SamulKyull.xyz>
2024-12-07 00:48:00 +0800

    [board] support clk

---
+++ src/drivers/pmu/axp8191.c
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <io.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <types.h>
+
+#include <common.h>
+#include <log.h>
+
+#include <pmu/axp.h>
+
+/* clang-format off */
+static axp_contrl_info axp_ctrl_tbl[] = {
+
+};
+/* clang-format on */
