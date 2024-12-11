6f892533c0cfca1d9601a4671fde28a352a830ed
SamulKyull <SamulKyull@SamulKyull.xyz>
2024-12-12 06:28:53 +0800

    [driver] add pwm support

---
+++ include/drivers/sys-trng.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __SYS_TRNG_H__
+#define __SYS_TRNG_H__
+
+#include <io.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <types.h>
+
+#include <sys-clk.h>
+#include <sys-gpio.h>
+
+
+
+#endif // __SYS_TRNG_H__
