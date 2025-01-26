59f95adfd278d6bfb715064b1819c346dacf2821
Zhouqi Jiang <luojia@hust.edu.cn>
2025-01-27 00:04:28 +0800

    [board] use syterkit::load_from_sdcard in 100ask-d1-h rust syterboot program
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- rust/src/sdcard.rs
+++ rust/src/sdcard.rs
@@ -1,10 +1,12 @@
 use embedded_sdmmc::{BlockDevice, TimeSource, VolumeManager};
 
+const MAX_LOAD_FILES: usize = 5;
+
 pub enum SdCardError<E: core::fmt::Debug> {
     OpenVolume(embedded_sdmmc::Error<E>),
     OpenRootDir(embedded_sdmmc::Error<E>),
     CloseRootDir(embedded_sdmmc::Error<E>),
-    LoadFile(),
+    LoadFile(heapless::Vec<embedded_sdmmc::Error<E>, MAX_LOAD_FILES>),
 }
 
 pub unsafe fn load_from_sdcard<D, T>(
@@ -24,8 +26,16 @@ where
         .open_root_dir(volume0)
         .map_err(SdCardError::OpenRootDir)?;
 
+    let mut load_file_errors = heapless::Vec::new();
     for (filename, addr, size) in files {
-        unsafe { load_file_into_memory(&mut volume_mgr, root_dir, filename, addr, size) }?;
+        let ans = unsafe { load_file_into_memory(&mut volume_mgr, root_dir, filename, addr, size) };
+        if let Err(e) = ans {
+            // discard extra errors
+            let _ = load_file_errors.push(e);
+        }
+    }
+    if load_file_errors.len() != 0 {
+        return Err(SdCardError::LoadFile(load_file_errors));
     }
 
     volume_mgr
@@ -42,33 +52,23 @@ unsafe fn load_file_into_memory<D: BlockDevice, T: TimeSource>(
     file_name: &str,
     addr: usize,
     max_size: u32,
-) -> Result<usize, SdCardError<D::Error>> {
+) -> Result<usize, embedded_sdmmc::Error<D::Error>> {
     // Find and open the file
-    volume_mgr
-        .find_directory_entry(dir, file_name)
-        .map_err(|_e| SdCardError::LoadFile())?;
+    volume_mgr.find_directory_entry(dir, file_name)?;
 
-    let file = volume_mgr
-        .open_file_in_dir(dir, file_name, embedded_sdmmc::Mode::ReadOnly)
-        .map_err(|_e| SdCardError::LoadFile())?;
+    let file = volume_mgr.open_file_in_dir(dir, file_name, embedded_sdmmc::Mode::ReadOnly)?;
 
     // Check file size
-    let file_size = volume_mgr
-        .file_length(file)
-        .map_err(|_e| SdCardError::LoadFile())?;
+    let file_size = volume_mgr.file_length(file)?;
     if file_size > max_size {
-        return Err(SdCardError::LoadFile());
+        return Err(embedded_sdmmc::Error::NotEnoughSpace);
     }
 
     // Read file content into memory
     let target = unsafe { core::slice::from_raw_parts_mut(addr as *mut u8, file_size as usize) };
-    let size = volume_mgr
-        .read(file, target)
-        .map_err(|_e| SdCardError::LoadFile())?;
+    let size = volume_mgr.read(file, target)?;
 
-    volume_mgr
-        .close_file(file)
-        .map_err(|_e| SdCardError::LoadFile())?;
+    volume_mgr.close_file(file)?;
 
     Ok(size)
 }
