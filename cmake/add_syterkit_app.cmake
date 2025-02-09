36dbdfdd542c5dcd2da37535f6b5ef68be6e8355
SamulKyull <SamulKyull@SamulKyull.xyz>
2025-02-10 07:59:02 +0800

    [board] remove payloads

--- cmake/add_syterkit_app.cmake
+++ cmake/add_syterkit_app.cmake
@@ -70,13 +70,13 @@ function(add_syterkit_app target_name)
     if(CMAKE_BUILD_TYPE STREQUAL Trace OR CMAKE_BUILD_TYPE STREQUAL Debug)
         add_custom_command(
             TARGET ${target_name}_bin
-            POST_BUILD COMMAND ${CROSS_COMPILE}objdump -S ${target_name}_bin > ${target_name}_bin.asm
+            POST_BUILD COMMAND ${CROSS_COMPILE}objdump -D ${target_name}_bin > ${target_name}_bin.asm
             COMMENT "Objdump ${target_name}_bin"
         )
 
         add_custom_command(
             TARGET ${target_name}_fel
-            POST_BUILD COMMAND ${CROSS_COMPILE}objdump -S ${target_name}_fel > ${target_name}_fel.asm
+            POST_BUILD COMMAND ${CROSS_COMPILE}objdump -D ${target_name}_fel > ${target_name}_fel.asm
             COMMENT "Objdump ${target_name}_fel"
         )
     endif()
