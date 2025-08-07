0965f7a90a1338ec66962157d9839fe3e7070a6d
SamulKyull <SamulKyull@SamulKyull.xyz>
2025-08-08 00:05:15 +0800

    board: add new avaota f2

---
+++ board/avaota-f2/app/main.c
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <types.h>
+
+#include <log.h>
+
+#include <sys-clk.h>
+#include <sys-dma.h>
+#include <sys-dram.h>
+#include <sys-gpio.h>
+#include <sys-i2c.h>
+#include <sys-sdcard.h>
+#include <sys-sdhci.h>
+#include <sys-spi-nand.h>
+#include <sys-spi-nor.h>
+#include <sys-spi.h>
+
+#include <common.h>
+
+#include <cli.h>
+#include <cli_shell.h>
+#include <cli_termesc.h>
+
+extern sunxi_serial_t uart_dbg;
+
+int main(void) {
+    sunxi_serial_init(&uart_dbg);
+
+    show_banner();
+
+    printk_info("Hello World!\n");
+
+    syterkit_shell_attach(NULL);
+
+    abort();
+
+    return 0;
+}
