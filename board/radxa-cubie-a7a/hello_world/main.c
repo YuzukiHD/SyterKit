c7e45edc196676203b9952460cf4b8c574852137
SamulKyull <SamulKyull@SamulKyull.xyz>
2025-08-07 23:55:51 +0800

    board: add some spi support

--- board/radxa-cubie-a7a/hello_world/main.c
+++ board/radxa-cubie-a7a/hello_world/main.c
@@ -14,11 +14,11 @@
 #include <sys-dram.h>
 #include <sys-sdcard.h>
 #include <sys-i2c.h>
+#include <sys-spi.h>
 
 extern sunxi_serial_t uart_dbg;
-
 extern sunxi_i2c_t i2c_pmu;
-
+extern sunxi_spi_t sunxi_spi0;
 extern sunxi_sdhci_t sdhci0;
 
 int main(void) {
@@ -35,6 +35,9 @@ int main(void) {
     pmu_axp8191_init(&i2c_pmu);
 
     pmu_axp8191_dump(&i2c_pmu);
+
+    // TODO
+    sunxi_spi_init(&sunxi_spi0);
 
     printk_info("Hello World!\n");
 
