21565bdb223ff9c868570991e29d82acbf2780d1
SamulKyull <SamulKyull@SamulKyull.xyz>
2025-11-16 08:18:17 +0800

    board: a7a support jlink openocd

--- src/drivers/chips/sun60iw2/sys-dram.c
+++ src/drivers/chips/sun60iw2/sys-dram.c
@@ -20,6 +20,8 @@
 
 extern sunxi_i2c_t i2c_pmu;
 
+static uint32_t dram_size;
+
 void sunxi_smc_en_with_glitch_workaround(void) {
 	return;
 }
@@ -28,6 +30,15 @@ int set_ddr_voltage_ext(char *name, int set_vol, int on) {
 	printk_debug("PMU: %s set vol %d, onoff %d\n", name, set_vol, on);
 	pmu_axp8191_set_vol(&i2c_pmu, name, set_vol, on);
 	return 0;
+}
+
+uint32_t sunxi_get_dram_size() {
+	return dram_size;
+}
+
+uint32_t sunxi_dram_init(void *para) {
+	dram_size = init_DRAM(0, para);
+	return dram_size;
 }
 
 uint32_t sunxi_dram_init(void *para) {
