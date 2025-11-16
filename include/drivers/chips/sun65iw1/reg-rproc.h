398d79902370ee3465b65321e98bf5ea1606cbd2
SamulKyull <SamulKyull@SamulKyull.xyz>
2025-11-16 09:07:56 +0800

    refactor(board): update avaota-m1 board configuration and drivers
    - Replace board_common_init with syterkit_shell_attach in main.c
    - Update memory address and length configurations in avaota-m1.cmake
    - Modify UART and SDHCI pin configurations in board.c
    - Remove unused clock initialization code in sys-clk.c
    - Update register definitions in reg-ncat.h and reg-rproc.h
    - Adjust chip identification and display information

--- include/drivers/chips/sun65iw1/reg-rproc.h
+++ include/drivers/chips/sun65iw1/reg-rproc.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 
-#ifndef __REG_RPROC_H__
-#define __REG_RPROC_H__
+#ifndef __SUN65IW1_REG_RPROC_H__
+#define __SUN65IW1_REG_RPROC_H__
 
 
-#endif// __REG_RPROC_H__
+#endif// __SUN65IW1_REG_RPROC_H__
