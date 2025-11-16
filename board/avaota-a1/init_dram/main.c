b6e27761798d2baae10a12a860d080c38a415b92
SamulKyull <SamulKyull@SamulKyull.xyz>
2025-11-16 08:18:17 +0800

    board: a7a support jlink openocd

--- board/avaota-a1/init_dram/main.c
+++ board/avaota-a1/init_dram/main.c
@@ -67,7 +67,7 @@ int main(void) {
 
 	enable_sram_a3();
 
-	uint32_t dram_size = sunxi_dram_init(&dram_para);
+	uint32_t dram_size = sunxi_dram_init(dram_para);
 	arm32_mmu_enable(SDRAM_BASE, dram_size);
 	printk_info("DRAM: DRAM Size = %dMB", dram_size);
 
