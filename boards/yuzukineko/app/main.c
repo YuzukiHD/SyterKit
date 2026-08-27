64c362a49d42cb4b23e1087d59dc8e711d8445d6
SamulKyull <SamulKyull@SamulKyull.com>
2026-08-27 14:17:18 +0000

    board: add SPI support and device tree configuration for SPI NOR flash

--- boards/yuzukineko/app/main.c
+++ boards/yuzukineko/app/main.c
@@ -31,6 +31,8 @@
 #include <cli/cli_shell.h>
 #include <cli/cli_termesc.h>
 #include <dt-compatible/psram-dt.h>
+#include <dt-compatible/spi-dt.h>
+#include <dt-compatible/spi-nor-dt.h>
 
 int main(void)
 {
@@ -56,6 +58,35 @@ int main(void)
 	sunxi_psram_init(&psram);
 
 	printk_info("LPSRAM Size = %d MB\n", sunxi_get_psram_size(&psram));
+#endif
+
+#ifdef CONFIG_DRIVER_SPI
+	sunxi_spi_t spi = { 0 };
+	spi_nor_t nor = { 0 };
+
+	if (sunxi_spi_dt_read_alias(&spi, "spi0", NULL) != DRIVER_OK ||
+	    spi_nor_dt_read_alias(&nor, "spi-nor0", &spi) != DRIVER_OK) {
+		printk_error("SPI: invalid devicetree configuration\n");
+		return -1;
+	}
+
+	sunxi_spi_init(&spi);
+
+	if (spi_nor_detect(&nor) != 0) {
+		printk_error("SPI NOR: no supported flash detected\n");
+		return -1;
+	}
+
+	/* Read the first 1MiB of the SPI NOR into PSRAM and report throughput. */
+	uint32_t nor_read_size = 1024 * 1024;
+	uint32_t time_start = time_ms();
+	spi_nor_read(&nor, (void *)SUNXI_PSRAM_BASE, 0x0, nor_read_size);
+	uint32_t time_end = time_ms();
+	uint32_t delta = (time_end - time_start);
+	if (delta == 0U)
+		delta = 1U;
+	printk_info("SPI NOR: read %uKiB in %ums, %uKiB/s\n", nor_read_size / 1024, delta, (nor_read_size / 1024) / delta);
+	dump_hex(SUNXI_PSRAM_BASE, 0x40);
 #endif
 
 	syterkit_shell_attach(NULL);
